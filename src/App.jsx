697112842226851a90247f0eb22c40f23fead460
amranhossain <amranhossainchy@gmail.com>
2023-10-08 18:52:04 +0600

    send data to server by post method

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,30 @@ function App() {
     .then(data => setUsers(data))
   } ,[])
 
+
+  const addUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const newUser = {name, email}
+    fetch('http://localhost:5000/users', {
+      method : 'POST',
+      headers : {
+        'content-type' : 'application/json'
+      },
+      body : JSON.stringify(newUser)
+    })
+    .then(res => res.json())
+    .then(data => {
+      setUsers([...users, data])
+      form.reset();
+    })
+  }
+
   return (
     <>
       <h1>User Management Client Site</h1>
-
       <div>
         {
           users.length
@@ -21,9 +41,15 @@ function App() {
         {
           users.map(user => <p key={user.id}> {user.name} </p>)
         }
-
         <div>
-
+          {/* input field  */}
+          <form onSubmit={addUser}>
+            <input type="text" name="name" id="" />
+            <br />
+            <input type="email" name="email" id="" />
+            <br />
+            <input type="submit" value="add user" />
+          </form>
         </div>
       </div>
     </>
